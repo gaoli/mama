cba11e88cf1c6dcdad16927c21052b14c2846862
高力 <3071730@qq.com>
2017-05-11 17:24:13 +0800

    优化页面跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,21 +2,11 @@ import AV from '../../libs/av';
 import Category from '../../model/category';
 
 Page({
-  data: {
-
-  },
   onLoad() {
     new AV.Query(Category).find().then((category) => {
       this.setData({
         category,
       });
-    });
-  },
-  openListPage(event) {
-    const { categoryId, categoryName } = event.currentTarget.dataset;
-
-    wx.navigateTo({
-      url: `../list/list?categoryId=${categoryId}&categoryName=${categoryName}`,
     });
   },
 });
