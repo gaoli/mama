b231b109434e9860a163587b0b9bd8cea1deec5b
高力 <3071730@qq.com>
2017-05-13 16:16:24 +0800

    添加搜索功能

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,62 @@
-Page({
+import AV from '../../libs/av';
+import Food from '../../model/food';
 
+Page({
+  data: {
+    value: '',
+    focus: true,
+    foods: [],
+  },
+
+  /**
+   * 显示输入框
+   */
+  showInput() {
+    this.setData({
+      focus: true,
+    });
+  },
+
+  /**
+   * 隐藏输入框
+   */
+  hideInput() {
+    wx.navigateBack();
+  },
+
+  /**
+   * 清空输入框
+   */
+  clearInput() {
+    this.setData({
+      value: '',
+      foods: [],
+    });
+  },
+
+  /**
+   * 处理值输入
+   */
+  handleTyping(e) {
+    const { value } = e.detail;
+
+    this.setData({
+      value,
+    });
+
+    if (value !== '') {
+      new AV.Query(Food)
+        .contains('name', value)
+        .find()
+        .then((foods) => {
+          this.setData({
+            foods,
+          });
+        });
+    } else {
+      this.setData({
+        foods: [],
+      });
+    }
+  },
 });
