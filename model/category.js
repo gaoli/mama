dfd8770bfc1868a926b9ae3286ebdf176495bdc7
高力 <3071730@qq.com>
2017-05-13 08:55:29 +0800

    食物类别数据本地获取

--- model/category.js
+++
@@ -1,9 +0,0 @@
-import AV from '../libs/av';
-
-class Category extends AV.Object {
-
-}
-
-AV.Object.register(Category);
-
-export default Category;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,44 @@
-import AV from '../../libs/av';
-import Category from '../../model/category';
-
 Page({
+  data: {
+    categories: [{
+      id: 1,
+      name: '水果',
+    }, {
+      id: 2,
+      name: '肉类',
+    }, {
+      id: 3,
+      name: '水产海鲜',
+    }, {
+      id: 4,
+      name: '饮品',
+    }, {
+      id: 5,
+      name: '零食小吃',
+    }, {
+      id: 6,
+      name: '干果',
+    }, {
+      id: 7,
+      name: '蔬菜',
+    }, {
+      id: 8,
+      name: '五谷杂粮',
+    }, {
+      id: 9,
+      name: '调味品',
+    }, {
+      id: 10,
+      name: '乳制品',
+    }, {
+      id: 11,
+      name: '粮食加工',
+    }, {
+      id: 12,
+      name: '药材',
+    }],
+  },
   onLoad() {
-    new AV.Query(Category).find().then((category) => {
-      this.setData({
-        category,
-      });
-    });
+
   },
 });
